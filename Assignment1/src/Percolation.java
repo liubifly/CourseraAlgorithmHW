52ee7c3d00b58c3f26a978d35a29b3dafab0a8c8
Bifei <liubifly@hotmail.com>
2018-12-16 12:42:13 -0500

    added gitignore and assignment 1

---
+++ Assignment1/src/Percolation.java
@@ -0,0 +1,38 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new java.lang.IllegalArgumentException("n should be greater than 0.");
+        }
+        else {
+
+        }
+    }
+
+    public void open(int row, int col) {
+
+    }
+
+    public boolean isOpen(int row, int col) {
+
+    }
+
+    public int numberOfOpenSites() {
+
+    }
+
+    public boolean percolates() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+}
